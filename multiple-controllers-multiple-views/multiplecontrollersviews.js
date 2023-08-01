c120c7e6eb097c05fa04c2a928ba476ef8a9b811
Ssxund3r <https://github.com/Ssxund3r>
2023-08-01 20:12:15 -0300

    Multiple Controllers and Views

---
+++ multiple-controllers-multiple-views/multiplecontrollersviews.js
@@ -0,0 +1,11 @@
+// Módulo
+var angularApp = angular.module('angularApp', []);
+
+// CONTROLLERS
+angularApp.controller('mainController', ['$scope', function ($scope) {
+    $scope.name = 'main';
+}]);
+
+angularApp.controller('secondController', ['$scope', function ($scope) {
+    $scope.name = 'Second';
+}]);
