e2218b3b9b057a7df7dee7732bb4cf1e367a54f5
Ssxund3r <https://github.com/Ssxund3r>
2023-08-06 19:25:16 -0300

    Criando o Service

--- creating-service/creating.js
+++ creating-service/creating.js
@@ -19,24 +19,34 @@ angularApp.config(function ($routeProvider) {
         })
 });
 
-angularApp.controller('mainController', ['$scope', '$log',
-    function ($scope, $log) {
+angularApp.controller('mainController', ['$scope', '$log', 'nameService',
+    function ($scope, $log, nameService) {
 
         $scope.name = 'Main';
+        $scope.name = nameService.name;
+
         $log.main = 'Property from main';
-        $log.log($log);
-        $log.$log($scope);
-        
+
+        $log.log(nameService.name);
+        $log.log(nameService.nameLenght());
+
+        $scope.$watch('name', function () {
+
+            nameService.name = $scope.name;
+        });
+
     }]);
 
-angularApp.controller('secondController', ['$scope', '$log', '$routeParams',
-    function ($scope, $log, $routeParams) {
+angularApp.controller('secondController', ['$scope', '$log', '$routeParams', 'nameService',
+    function ($scope, $log, $routeParams, nameService) {
 
         $scope.num = $routeParams.num || 1;
-        
+
         $log.second = 'Property from second';
         $log.log($log);
         $log.$log($scope);
+
+        $scope.name = nameService.name;
 
     }]);
 
@@ -46,3 +56,13 @@ angularApp.controller('secondController', ['$scope', '$log', '$routeParams',
         $scope.name = 'Second';
 
     }]);
+
+angularApp.service('nameService', function () {
+    var self = this;
+
+    this.name = 'Gabriel Costa';
+    this.nameLenght = function () {
+
+        return self.name.length;
+    }
+});
